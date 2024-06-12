50c7d713bdacd811525cdf8d72da2bfa5be1ed81
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2024-06-12 13:35:36 +0000

    Add theme and color customisation

---
+++ lib/theme/custom_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color borderColor = Color(0xffebebeb);
+  static const Color backgroundColor = Color(0xfff4f5f9);
+  static const Color lightGrayBackgroundColor = Color(0xfff5f5f5);
+  static const Color linkColor = Color(0xff407ec7);
+  static const Color grayTextColor = Color(0xff868889);
+  static const Color primaryColor = Color(0xffaedc81);
+  static const Color primaryDarkColor = Color(0xff6cc51d);
+  static const Color primaryLightColor = Color(0xffebffd7);
+}
